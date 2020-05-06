e58d53e4d49d67ab4a299e214d385de5dbdd8eaa
Chris <chris.page@samtec.com>
2020-05-06 20:16:50 +0100

    update build script and compose

--- scripts/balena_build_deploy.sh
+++ scripts/balena_build_deploy.sh
@@ -5,11 +5,11 @@ cd $DIR/..
 
 AARCH=${BUILD_ARCH,:-'armv7hf'}
 if [ "$AARCH" = 'aarch64' ]; then 
-    balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'SWITCHER_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
-    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'SWITCHER_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' hdr-switcher
+    balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
+    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' hdr-switcher
 else
-    balena build --deviceType raspberrypi4-64 --arch armv7hf -B 'SWITCHER_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7'
-    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'SWITCHER_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' hdr-switcher
+    balena build --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7'
+    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' hdr-switcher
 fi
 
 
