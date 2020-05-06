d4588924e13caa766eb32fea279e0aaad63cf1d4
Chris <chris.page@samtec.com>
2020-05-06 21:49:10 +0100

    use udef, fix script application

--- scripts/balena_build_deploy.sh
+++ scripts/balena_build_deploy.sh
@@ -3,13 +3,13 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/.. 
 
-AARCH=${BUILD_ARCH,:-'armv7hf'}
+AARCH=${BUILD_ARCH,:-'aarch64'}
 if [ "$AARCH" = 'aarch64' ]; then 
     #balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
-    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' hdr-switcher
+    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' jupyter
 else
     #balena build --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7'
-    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' hdr-switcher
+    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' jupyter
 fi
 
 
