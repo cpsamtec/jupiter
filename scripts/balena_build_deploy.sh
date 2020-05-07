8e97b43045437043eb7f0d392d7ff4b0b8c590ac
Chris <chris.page@samtec.com>
2020-05-07 18:04:35 +0100

    update dockerfile

--- scripts/balena_build_deploy.sh
+++ scripts/balena_build_deploy.sh
@@ -3,7 +3,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/.. 
 
-AARCH=${BUILD_ARCH,:-'aarch64'}
+AARCH=${BUILD_ARCH:='aarch64'}
 if [ "$AARCH" = 'aarch64' ]; then 
     #balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
     balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' jupyter
