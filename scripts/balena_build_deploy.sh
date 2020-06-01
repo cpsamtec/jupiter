4ab968780f0536a48fee45169c4365ea578b8dbf
Chris Page <cp@concerco.com>
2020-06-01 11:59:49 -0400

    using python 3.8 base image

--- scripts/balena_build_deploy.sh
+++ scripts/balena_build_deploy.sh
@@ -4,12 +4,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/.. 
 
 AARCH=${BUILD_ARCH:='aarch64'}
+SYSTEM_ARCH=$(uname -m)
 if [ "$AARCH" = 'aarch64' ]; then 
-    balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
-    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' jupyter
+    if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
+        #docker-compose build --build-arg 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' --build-arg 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' notebook samtecdeviceshare
+        docker-compose build --build-arg NOTEBOOK_BASE_IMAGE=python:3.8-buster --build-arg 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' notebook samtecdeviceshare
+    else
+        balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
+    fi
+    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' jupyter
 else
-    balena build --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7'
-    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=balenalib/armv7hf-debian-python:3.7' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' jupyter
+    if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
+        docker-compose build --build-arg 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' --build-arg 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' notebook samtecdeviceshare
+    else 
+        balena build --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7'
+    fi
+    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' jupyter
 fi
 
 
