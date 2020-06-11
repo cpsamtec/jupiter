48aa4ff21f1da6795f910a74de9e34aa01c43a48
Chris Page <cp@concerco.com>
2020-06-11 09:50:58 -0400

    update credential logging

--- notebook/credentials.sh
+++ notebook/credentials.sh
@@ -1,16 +1,16 @@
 #/usr/bin/env bash
 
-echo "/lab token is"
+echo "getting credentials"
 token=$(jupyter notebook list | grep token | sed -n '0,/http/s/.*token=\([a-zA-Z0-9]\+\).*/\1/p')
 
 echo "jupyter lab token is ${token}"
 echo "jupyter lab located locally at"
-echo "http://${BALENA_DEVICE_UUID:0:7}.local/lab"
+echo "http://${BALENA_DEVICE_UUID:0:7}.local/lab?token=${token}"
 echo "jupyter lab located remotely at"
-echo "https://${BALENA_DEVICE_UUID}.balena-devices.com/lab"
+echo "https://${BALENA_DEVICE_UUID}.balena-devices.com/lab?token=${token}"
 code_pass=$(cat /program/code-server.yml | sed -n 's/^password: \([a-zA-Z0-9]\+\)/\1/p')
 echo "code server password is ${code_pass}"
 echo "code located locally at"
-echo "http://${BALENA_DEVICE_UUID:0:7}.local/code"
+echo "http://${BALENA_DEVICE_UUID:0:7}.local/code/login?password=${code_pass}"
 echo "code server located remotely at"
-echo "https://${BALENA_DEVICE_UUID}.balena-devices.com/code"
+echo "https://${BALENA_DEVICE_UUID}.balena-devices.com/code/login?password=${code_pass}"
