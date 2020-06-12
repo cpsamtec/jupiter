7dcbcf783ecbc29dd6dc81c21f2a7ec2390ee7cc
Chris Page <cp@concerco.com>
2020-06-12 11:49:26 -0400

    deploy add amd64/x86_64 project

--- scripts/balena_build_deploy.sh
+++ scripts/balena_build_deploy.sh
@@ -5,17 +5,24 @@ cd $DIR/..
 
 bash scripts/create-local-docker-compose.sh 
 
-AARCH=${BUILD_ARCH:='aarch64'}
+SERVICES="samtecdeviceshare nginx notebook minio"
+DEPLOY_ARCH=${DEPLOY_ARCH:='aarch64'}
 SYSTEM_ARCH=$(uname -m)
-if [ "$AARCH" = 'aarch64' ]; then 
+if [ "$DEPLOY_ARCH" = 'aarch64' ]; then 
     if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
-        #docker-compose build --build-arg 'NOTEBOOK_BASE_IMAGE=balenalib/aarch64-debian-python:3.7' --build-arg 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' notebook samtecdeviceshare
-        docker-compose -f docker-compose-local.yml build --build-arg NOTEBOOK_BASE_IMAGE=python:3.8-buster --build-arg 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' samtecdeviceshare nginx notebook minio
+        docker-compose -f docker-compose-local.yml build --build-arg NOTEBOOK_BASE_IMAGE=python:3.8-buster --build-arg 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' ${SERVICES} 
     else
         balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
     fi
     balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' jupyter
-else
+elif [ "$DEPLOY_ARCH" = 'amd64' ]; then 
+    if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
+        docker-compose --project-name jupyter-x86 build --build-arg NOTEBOOK_BASE_IMAGE=python:3.8-buster --build-arg 'SDS_BASE_IMAGE=python:3.8-alpine' ${SERVICES}
+    else
+        balena build --projectName jupyter-x86 --deviceType intel-nuc --arch amd64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=python:3.8-alpine'
+    fi
+    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=python:3.8-alpine' jupyter-x86
+else #arm7
     if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
         docker-compose -f docker-compose-local.yml build --build-arg 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' --build-arg 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' samtecdeviceshare nginx notebook minio
     else 
