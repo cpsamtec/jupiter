f6647211cc52cb9b0b758ff735158ddb95469e85
Chris Page <chris.page@samtec.com>
2020-06-13 16:59:14 -0400

    fix architecture for base images

--- scripts/balena_build_deploy.sh
+++ scripts/balena_build_deploy.sh
@@ -8,27 +8,36 @@ bash scripts/create-local-docker-compose.sh
 SERVICES="samtecdeviceshare nginx notebook minio"
 DEPLOY_ARCH=${DEPLOY_ARCH:='aarch64'}
 SYSTEM_ARCH=$(uname -m)
+BUILD_ARGS="BUILD_ARG BASE_IMAGE_ARCH=DOCKER_ARCH BUILD_ARG NOTEBOOK_BASE_IMAGE=DOCKER_ARCH/python:3.8-buster BUILD_ARG SDS_BASE_IMAGE=DOCKER_ARCH/python:3.8-alpine"
+COMPOSE_BUILD_ARGS=${BUILD_ARGS//BUILD_ARG/--build-arg}
+BALENA_BUILD_ARGS=${BUILD_ARGS//BUILD_ARG/-B}
 if [ "$DEPLOY_ARCH" = 'aarch64' ]; then 
+    COMPOSE_BUILD_ARGS=${COMPOSE_BUILD_ARGS//DOCKER_ARCH/arm64v8}
+    BALENA_BUILD_ARGS=${BALENA_BUILD_ARGS//DOCKER_ARCH/arm64v8}
     if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
-        docker-compose -f docker-compose-local.yml build --build-arg NOTEBOOK_BASE_IMAGE=python:3.8-buster --build-arg 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' ${SERVICES} 
+        docker-compose -f docker-compose-aarch64.yml build ${COMPOSE_BUILD_ARGS} ${SERVICES} 
     else
-        balena build --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7'
+        balena build --deviceType raspberrypi4-64 --arch aarch64 ${BALENA_BUILD_ARGS} 
     fi
-    balena deploy --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/aarch64-alpine-python:3.7' jupyter
+    balena deploy --deviceType raspberrypi4-64 --arch aarch64 ${BALENA_BUILD_ARGS} jupyter
 elif [ "$DEPLOY_ARCH" = 'amd64' ]; then 
+    COMPOSE_BUILD_ARGS=${COMPOSE_BUILD_ARGS//DOCKER_ARCH/amd64}
+    BALENA_BUILD_ARGS=${BALENA_BUILD_ARGS//DOCKER_ARCH/amd64}
     if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
-        docker-compose --project-name jupyter-x86 build --build-arg NOTEBOOK_BASE_IMAGE=python:3.8-buster --build-arg 'SDS_BASE_IMAGE=python:3.8-alpine' ${SERVICES}
+        docker-compose --project-name jupyter-x86 build ${COMPOSE_BUILD_ARGS} ${SERVICES}
     else
-        balena build --projectName jupyter-x86 --deviceType intel-nuc --arch amd64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=python:3.8-alpine'
+        balena build --projectName jupyter-x86 --deviceType intel-nuc --arch amd64 ${BALENA_BUILD_ARGS} 
     fi
-    balena deploy --project-name jupyter-x86 --deviceType raspberrypi4-64 --arch aarch64 -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=python:3.8-alpine' jupyter-x86
+    balena deploy --project-name jupyter-x86 --deviceType intel-nuc --arch amd64 ${BALENA_BUILD_ARGS} jupyter-x86
 else #arm7
+    COMPOSE_BUILD_ARGS=${COMPOSE_BUILD_ARGS//DOCKER_ARCH/arm32v7}
+    BALENA_BUILD_ARGS=${BALENA_BUILD_ARGS//DOCKER_ARCH/arm32v7}
     if [ "$SYSTEM_ARCH" = 'x86_64' ]; then 
-        docker-compose -f docker-compose-local.yml build --build-arg 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' --build-arg 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' samtecdeviceshare nginx notebook minio
+        docker-compose -f docker-compose-armv7hf.yml build ${BALENA_BUILD_ARGS} ${SERVICES}
     else 
-        balena build --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7'
+        balena build --deviceType raspberrypi4-64 --arch armv7hf ${BALENA_BUILD_ARGS} 
     fi
-    balena deploy --deviceType raspberrypi4-64 --arch armv7hf -B 'NOTEBOOK_BASE_IMAGE=python:3.8-buster' -B 'SDS_BASE_IMAGE=balenalib/armv7hf-alpine-python:3.7' jupyter
+    balena deploy --deviceType raspberrypi4-64 --arch armv7hf ${BALENA_BUILD_ARGS} jupyter
 fi
 
 
--- scripts/create-local-docker-compose.sh
+++ scripts/create-local-docker-compose.sh
@@ -2,4 +2,7 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/.. 
-cat docker-compose.yml | sed -e 's/2.0/2.4/' -e 's/#platform/platform/' -e 's/#dockerfile/dockerfile/' > docker-compose-local.yml
+cat docker-compose.yml | sed 's/#dockerfile/dockerfile/' > docker-compose-aarch64.yml
+cat docker-compose.yml | sed 's/#dockerfile.*/dockerfile: Dockerfile.arm.release/' > docker-compose-armv7hf.yml
+#cat docker-compose.yml | sed -e 's/2.0/2.4/' -e 's/#platform/platform/' -e 's/#dockerfile/dockerfile/' > docker-compose-aarch64.yml
+#cat docker-compose.yml | sed -e 's/2.0/2.4/' -e 's!#platform: '\''linux/arm64'\''!platform: '\''linux/amd64'\''!' > docker-compose-local-amd64.yml
