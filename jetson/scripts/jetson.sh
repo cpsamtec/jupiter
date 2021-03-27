01626e22d73e6533cb617956bd9a696c83f5a3c2
Chris Page <chris.page@samtec.com>
2021-03-27 14:19:42 -0400

    update scripts and change nb base image name variable

--- jetson/scripts/jetson.sh
+++ jetson/scripts/jetson.sh
@@ -4,11 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ProgName=$(basename $0)
 cd $DIR/.. 
 echo "from jetson dir located at: $(pwd)"
-JUPI_NOTEBOOK_BASE_REPOSITORY=${JUPI_NOTEBOOK_BASE_REPOSITORY:-samtecdistro}
+JUPI_REPOSITORY=${JUPI_REPOSITORY:-samtecdistro}
 JUPI_NOTEBOOK_BASE_IMAGE=${JUPI_NOTEBOOK_BASE_IMAGE:-jetson}
 JUPI_NOTEBOOK_BASE_TAG=${JUPI_NOTEBOOK_BASE_TAG:-latest}
-JUPI_JETSON_TYPE=${JUPI_JETSON_TYPE:-xavier}
-export JUPI_NOTEBOOK_BASE_NAME=${JUPI_NOTEBOOK_BASE_REPOSITORY}/${JUPI_NOTEBOOK_BASE_IMAGE}:${JUPI_NOTEBOOK_BASE_TAG}
+export JUPI_NOTEBOOK_BASE_NAME=${JUPI_REPOSITORY}/${JUPI_NOTEBOOK_BASE_IMAGE}:${JUPI_NOTEBOOK_BASE_TAG}
 export JUPI_PROJECT_NAME=jupiter-jetson
 
 sub_help(){
@@ -21,10 +20,13 @@ sub_help(){
   
 sub_build(){
     docker build -t ${JUPI_NOTEBOOK_BASE_IMAGE} .
+    docker tag "${JUPI_NOTEBOOK_BASE_IMAGE}:${JUPI_NOTEBOOK_BASE_TAG}" "${JUPI_NOTEBOOK_BASE_NAME}"
     ${DIR}/../../scripts/jupiter.sh build aarch64
 }
   
 sub_deploy(){
+    docker tag "${JUPI_PROJECT_NAME}_notebook:latest" "${JUPI_REPOSITORY}/jupiter-notebook-jetson:latest"
+    docker push "${JUPI_REPOSITORY}/jupiter-notebook-jetson:latest"
     ${DIR}/../../scripts/jupiter.sh deploy aarch64
 }
 
