6e0482bf628dacc88a059a7df17dd6a08fbf171d
Chris Page <chris.page@samtec.com>
2021-04-14 17:27:21 -0400

    replace env script with jupiter-compose that will create alias to run locally as opposed to docker-compose command

---
+++ scripts/jupiter-compose-env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash 
+PREV_DIR=$DIR
+PREV_COMPOSE_PROJECT_NAME=$COMPOSE_PROJECT_NAME
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+case "$(uname -m)" in
+    "x86_64")
+        COMPOSE_PROJECT_NAME="${JUPI_PROJECT_NAME:-jupiter-amd64}"
+    ;;
+    "aarch64")
+        COMPOSE_PROJECT_NAME="${JUPI_PROJECT_NAME:-jupiter-aarch64}"
+    ;;
+    *)
+        echo "invalid ssytem architecture"
+        echo "valid include - amd64 | aarch64" 
+        ;;
+esac
+if [ -e "${DIR}/../docker-compose-local-override.yml" ]; then 
+    alias jupiter-compose="COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} docker-compose -f docker-compose.yml -f docker-compose-local-override.yml"
+else 
+    alias jupiter-compose="COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME} docker-compose -f docker-compose.yml"
+fi
+
+DIR=$PREV_DIR
+COMPOSE_PROJECT_NAME=$PREV_COMPOSE_PROJECT_NAME
+
+
